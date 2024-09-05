4e705a1a2ed0ac0c8f9916a29bc78310b8332892
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-09-05 18:09:04 +0200

    update

--- url/urls.py
+++ url/urls.py
@@ -5,7 +5,7 @@ from .views import url_view, redirect_view
 
 
 urlpatterns = [
-    path('url/', url_view, name='url_shortener'),
+    path('', url_view, name='url_shortener'),
     path('<str:short_url>/', redirect_view, name='redirect_view')
 
 
